432cd97d7eab290eb123816da1e4e0ebd70572c2
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-10-03 16:00:12 +0100

    NavbarCreate

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,5 +1,6 @@
 import { RobotoText } from '@/components/atoms/roboto_text'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
@@ -14,6 +15,51 @@ export default function Header() {
         priority
         className="h-full max-h-[700px] w-full"
       />
+      <nav className="absolute top-0 flex w-full items-center justify-center gap-1 pt-16 md:gap-6 lg:gap-20">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_IMAGES_PATH}images/logo.avif`}
+          alt="plane flying image"
+          width={130}
+          height={130}
+          unoptimized
+          priority
+        />
+        <Link href={'/'}>
+          <RobotoText
+            text="Inicio"
+            fontSize="20px"
+            className="text-white hover:text-blue-700"
+          />
+        </Link>
+        <Link href={'/'}>
+          <RobotoText
+            text="Sobre Nosotros"
+            fontSize="20px"
+            className="text-white hover:text-blue-700"
+          />
+        </Link>
+        <Link href={'/'}>
+          <RobotoText
+            text="Servicios"
+            fontSize="20px"
+            className="text-white hover:text-blue-700"
+          />
+        </Link>
+        <Link href={'/'}>
+          <RobotoText
+            text="Aerolineas y flota"
+            fontSize="20px"
+            className="text-white hover:text-blue-700"
+          />
+        </Link>
+        <Link href={'/'}>
+          <RobotoText
+            text="Contacto"
+            fontSize="20px"
+            className="text-white hover:text-blue-700"
+          />
+        </Link>
+      </nav>
       {/* header text */}
       <div className="absolute top-2/4 flex flex-col gap-2 pb-10 pl-10 pt-0 md:pl-32 md:pt-24 lg:pl-48 lg:pt-32">
         {/* title */}
