9e5c281f0d1def732d114ca5e1fbe9dc2c8e85e1
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-10-10 16:49:04 +0100

    tailwindConfigUpdate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        blue: '#0284c7',
       },
       maxWidth: {
         '8xl': '1850px',
