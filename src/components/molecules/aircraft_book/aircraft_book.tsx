fcc1a03b0daaa6d6d9246cb1b7fb6cc2737020dc
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-10-26 13:17:53 +0100

    MinorUpdates + RentPlaneModal + WarningIfUserIsNotLoggedModal

--- src/components/molecules/aircraft_book/aircraft_book.tsx
+++ src/components/molecules/aircraft_book/aircraft_book.tsx
@@ -14,14 +14,6 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.css'
 
 // Define interfaces for your data
-interface Airlines {
-  id: number
-  name: string
-  country: string
-  email: string
-  rental_role: string
-}
-
 interface Aircrafts {
   id: number
   airline_id: number
@@ -33,32 +25,25 @@ interface Aircrafts {
 }
 
 export default function AircraftBook() {
-  const [airlines, setAirlines] = useState<Airlines[]>([])
   const [aircrafts, setAircrafts] = useState<Aircrafts[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [selectedAircraft, setSelectedAircraft] = useState<Aircrafts | null>(
     null,
   )
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isLogInModalOpen, setLogInModalOpen] = useState(false)
   const [isUserSignedIn, setIsUserSignedIn] = useState(false)
 
   useEffect(() => {
-    // Check if user token is present (simple authentication check)
     const token = localStorage.getItem('authToken')
     setIsUserSignedIn(!!token)
 
-    // Fetch airline and aircraft data
     const fetchData = async () => {
       try {
-        const responseAirlines = await fetch(
-          process.env.NEXT_PUBLIC_API_URL + '/airlines',
-        )
         const responseAircrafts = await fetch(
           process.env.NEXT_PUBLIC_API_URL + '/aircrafts',
         )
-        const dataAirlines = await responseAirlines.json()
         const dataAircrafts = await responseAircrafts.json()
-        setAirlines(Array.isArray(dataAirlines) ? dataAirlines : [])
         setAircrafts(Array.isArray(dataAircrafts) ? dataAircrafts : [])
         setIsLoading(false)
       } catch (error) {
@@ -69,11 +54,9 @@ export default function AircraftBook() {
     fetchData()
   }, [])
 
-  // Handle aircraft rental click
   const handleAircraftClick = (aircraft: Aircrafts) => {
     if (!isUserSignedIn) {
-      alert('Please sign in to rent a plane')
-      return
+      setLogInModalOpen(true)
     }
     setSelectedAircraft(aircraft)
     setIsModalOpen(true)
@@ -81,11 +64,12 @@ export default function AircraftBook() {
 
   const closeModal = () => {
     setIsModalOpen(false)
+    setLogInModalOpen(false)
     setSelectedAircraft(null)
   }
 
   return (
-    <section className="flex flex-col items-center justify-center bg-gray-100 py-10">
+    <section className="relative flex flex-col items-center justify-center py-10">
       {isLoading ? (
         <div className="flex h-40 flex-col items-center justify-center">
           <Bars
@@ -98,65 +82,70 @@ export default function AircraftBook() {
           <RobotoText text="Cargando..." fontSize="20px" style="bold" />
         </div>
       ) : (
-        <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-          spaceBetween={20}
-          slidesPerView={'auto'}
-          loop={true}
-          speed={3000}
-          autoplay={{ delay: 0 }}
+        <div
+          className={`${isModalOpen ? 'pointer-events-none opacity-50 blur-sm' : ''}`}
         >
-          {aircrafts.map((aircraft) => (
-            <SwiperSlide
-              key={aircraft.id}
-              className="w-full max-w-[300px]"
-              onClick={() => handleAircraftClick(aircraft)}
-            >
-              <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-lg">
-                <RobotoText text={aircraft.type} fontSize="18px" style="bold" />
-                <img
-                  src={aircraft.aircraft_image_url}
-                  alt={aircraft.type}
-                  className="mt-3 h-48 w-full rounded-lg object-cover shadow-sm"
-                />
-                <RobotoText
-                  text={`Capacidad: ${aircraft.capacity} pasajeros`}
-                  fontSize="18px"
-                  className="mt-2 text-center text-gray-500"
-                />
-                <RobotoText
-                  text={`Precio por hora: ${aircraft.price_per_hour} €`}
-                  fontSize="18px"
-                  className="mt-1 text-center text-gray-500"
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+            spaceBetween={20}
+            slidesPerView={'auto'}
+            loop={true}
+            speed={3000}
+            autoplay={{ delay: 0 }}
+          >
+            {aircrafts.map((aircraft) => (
+              <SwiperSlide
+                key={aircraft.id}
+                className="w-full max-w-[300px]"
+                onClick={() => handleAircraftClick(aircraft)}
+              >
+                <div className="flex flex-col items-center rounded-lg bg-white p-4 shadow-lg">
+                  <RobotoText
+                    text={aircraft.type}
+                    fontSize="18px"
+                    style="bold"
+                  />
+                  <img
+                    src={aircraft.aircraft_image_url}
+                    alt={aircraft.type}
+                    className="mt-3 h-48 w-full rounded-lg object-cover shadow-sm"
+                  />
+                  <RobotoText
+                    text={`Capacidad: ${aircraft.capacity} pasajeros`}
+                    fontSize="18px"
+                    className="mt-2 text-center text-gray-500"
+                  />
+                  <RobotoText
+                    text={`Precio por hora: ${aircraft.price_per_hour} €`}
+                    fontSize="18px"
+                    className="mt-1 text-center text-gray-500"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       )}
 
-      <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
-        <div className="flex flex-col items-center p-4">
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+        className="relative p-0"
+      >
+        <div className="flex w-full max-w-lg flex-col items-center justify-center rounded-lg bg-white/80 p-4">
           <RobotoText
             text={`Alquilar el avión ${selectedAircraft?.type}`}
             fontSize="20px"
             style="bold"
           />
-          <form className="mt-4 flex flex-col">
-            <label className="mt-4">
-              <RobotoText
-                text="Fecha de alquiler"
-                fontSize="20px"
-                style="bold"
-              />
+          <form className="mt-1 flex flex-col gap-2">
+            <label className="pt-2">
+              <RobotoText text="Fecha de alquiler" fontSize="16px" />
               <input type="date" className="mt-2 rounded-md border p-2" />
             </label>
             <label>
-              <RobotoText
-                text="Duración del alquiler"
-                fontSize="20px"
-                style="bold"
-              />
+              <RobotoText text="Duración del alquiler" fontSize="16px" />
               <input
                 type="number"
                 className="mt-2 rounded-md border p-2"
@@ -166,8 +155,7 @@ export default function AircraftBook() {
             <label>
               <RobotoText
                 text="Introduzca el trayecto previsto"
-                fontSize="20px"
-                style="bold"
+                fontSize="16px"
               />
               <input
                 type="text"
@@ -175,20 +163,36 @@ export default function AircraftBook() {
                 placeholder="ej: ACE-BCN"
               />
             </label>
-            <button type="submit" className="mt-4 rounded-md bg-blue p-2">
-              <RobotoText
-                text="Enviar solicitud"
-                fontSize="20px"
-                style="bold"
-              />
-            </button>
           </form>
+          <button
+            type="submit"
+            className="mt-4 w-fit rounded-md bg-blue p-2 text-white"
+          >
+            <RobotoText text="Enviar solicitud" fontSize="16px" />
+          </button>
           <button
             onClick={closeModal}
             className="mt-4 rounded-md bg-red-500 p-2 text-white"
           >
-            <RobotoText text="Cerrar" fontSize="20px" style="bold" />
+            <RobotoText text="Cerrar" fontSize="16px" />
           </button>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={isLogInModalOpen}
+        onRequestClose={closeModal}
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+        className="relative p-0"
+      >
+        <div className="flex w-full max-w-lg flex-col items-center justify-center rounded-lg bg-white/80 p-4">
+          <RobotoText
+            text={
+              'Debes <a class="underline" href="/login"> iniciar sesión</a> para alquilar un avión'
+            }
+            fontSize="20px"
+            style="bold"
+            className="text-red-500"
+          />
         </div>
       </Modal>
     </section>
